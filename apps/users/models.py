9b52e28656616d404f77542acaccc348ef456c57
iSk2y <isk2yroot@gmail.com>
2018-11-03 14:11:11 +0800

    修改UserProfile中gender最大长度

--- apps/users/models.py
+++ apps/users/models.py
@@ -13,14 +13,14 @@ class UserProfile(AbstractUser):
     """
     # 性别choices
     GENDER_CHOICES = (
-        ('male','男'),
-        ('female','女')
+        ('male', '男'),
+        ('female', '女')
     )
-    nick_name = models.CharField(max_length=50,verbose_name="昵称",default='')
+    nick_name = models.CharField(max_length=50, verbose_name="昵称",default='')
     birthday = models.DateTimeField(verbose_name='生日',null=True,blank=True) # blank是表单验证，null是数据库范畴
-    gender = models.CharField(max_length=5,verbose_name="性别",choices=GENDER_CHOICES,default='female')
-    address = models.CharField(max_length=100,verbose_name="地址",default='')
-    mobile = models.CharField(max_length=11,null=True,blank=True)
+    gender = models.CharField(max_length=6, verbose_name="性别", choices=GENDER_CHOICES, default='female')
+    address = models.CharField(max_length=100, verbose_name="地址",default='')
+    mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(
         upload_to="image/%Y/%m",
         default="image/default.png",
