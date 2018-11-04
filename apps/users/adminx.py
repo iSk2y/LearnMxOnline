f62cdd8547a09432073be963caa1a27198ed27d6
iSk2y <isk2yroot@gmail.com>
2018-11-04 12:56:18 +0800

    配置xadmin，开启主题，配置app名，修改site名

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -4,9 +4,23 @@
 
 
 import xadmin
-
+from xadmin import views
 
 from .models import EmailVerifyRecord, Banner
+
+
+#  Xadmin管理配置类
+class BaseSetting(object):
+    # 开启主题
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = 'MxOnline'
+    site_footer = "MxOnline Platform"
+    # 收起菜单
+    menu_style = "accordion"
 
 
 class EmailVerifyRecordAdmin(object):
@@ -27,5 +41,7 @@ class BannerAdmin(object):
     list_filter = ['title', 'image', 'url', 'index', 'add_time']
 
 
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
