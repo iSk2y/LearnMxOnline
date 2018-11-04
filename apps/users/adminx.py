6e4bbd0fd26588a51056ae6dbd2f42471407ae12
iSk2y <isk2yroot@gmail.com>
2018-11-04 09:40:30 +0800

    两张表注册进xadmin，配置显示搜索过滤

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -2,3 +2,30 @@
 # -*- coding:utf-8 -*-
 #    @Author:iSk2y
 
+
+import xadmin
+
+
+from .models import EmailVerifyRecord, Banner
+
+
+class EmailVerifyRecordAdmin(object):
+    """
+    验证码的配置类
+    """
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    """
+    轮播图的配置类
+    """
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
