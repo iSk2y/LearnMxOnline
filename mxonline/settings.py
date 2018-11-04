73b3dee23fe91a11821a3bce194858cbe09e9017
iSk2y <isk2yroot@gmail.com>
2018-11-04 14:12:59 +0800

    引入静态文件，完成login和index显示页面

--- mxonline/settings.py
+++ mxonline/settings.py
@@ -130,4 +130,9 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 AUTH_USER_MODEL = "users.UserProfile"
--- mxonline/urls.py
+++ mxonline/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+
 import xadmin
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login')
 ]
